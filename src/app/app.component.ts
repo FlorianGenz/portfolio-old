ced0c06e845a7fa31e4b04f66505c4c227b11982
Florian <floriangenz@icloud.com>
2023-06-23 12:30:27 +0200

    first website commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  public log(event: any) {
+      document.getElementById("cursor")!.style.opacity = "1";
+      document.getElementById("cursor")!.style.left = event.x  + "px";
+      document.getElementById("cursor")!.style.top = event.y  + "px";
+  }
+
+  hideMouse(){
+    document.getElementById("cursor")!.style.opacity = "0";
+  }
+
+  changeMouse(isSmall: boolean){
+    if (isSmall){
+    document.getElementById("cursor")!.classList.add("bigCursor")
+    }else{
+      document.getElementById("cursor")!.classList.remove("bigCursor");
+    }
+  }
+
+  mouseEnterText(){
+    document.getElementById("cursor")!.classList.add("hoverCursor");
+  }
+
+  mouseLeaveText(){
+    document.getElementById("cursor")!.classList.remove("hoverCursor");
+  }
 }
