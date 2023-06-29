cdd1417568fbe97f9cafb6df66b74e8316284256
Florian <floriangenz@icloud.com>
2023-06-29 16:37:43 +0200

    added button to toggle darkmode

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent {
+
+  darkmode: boolean = true;
+
   public log(event: any) {
     document.getElementById("cursor")!.style.opacity = "1";
     document.getElementById("cursor")!.style.left = event.x  + "px";
@@ -30,6 +33,32 @@ export class PortfolioComponent {
 
   mouseLeaveText(){
     document.getElementById("cursor")!.classList.remove("hoverCursor");
+  }
+
+  changeBrightnessMode(){
+    if (this.darkmode){
+      let temp;
+      temp = getComputedStyle(document.documentElement).getPropertyValue("--backgroundColor");
+      document.documentElement.style.setProperty("--backgroundColor", getComputedStyle(document.documentElement).getPropertyValue("--fontColor"));
+      document.documentElement.style.setProperty("--fontColor", temp);
+
+      document.getElementById("home")!.style.content = "url('/assets/svg/home-iconDark.svg')";
+      document.getElementById("gitHub")!.style.content = "url('/assets/svg/gitHubDark.svg')";
+      document.getElementById("mail")!.style.content = "url('/assets/svg/mailDark.svg')";
+      document.getElementById("darkBrightMode")!.style.content = "url('/assets/svg/sunDark.svg')";
+      this.darkmode = false;
+    }else{
+      let temp;
+      temp = getComputedStyle(document.documentElement).getPropertyValue("--backgroundColor");
+      document.documentElement.style.setProperty("--backgroundColor", getComputedStyle(document.documentElement).getPropertyValue("--fontColor"));
+      document.documentElement.style.setProperty("--fontColor", temp);
+
+      document.getElementById("home")!.style.content = "url('/assets/svg/home-iconWhite.svg')";
+      document.getElementById("gitHub")!.style.content = "url('/assets/svg/gitHubWhite.svg')";
+      document.getElementById("mail")!.style.content = "url('/assets/svg/mailWhite.svg')";
+      document.getElementById("darkBrightMode")!.style.content = "url('/assets/svg/sunWhite.svg')";
+      this.darkmode = true;
+    }
   }
 
 }
